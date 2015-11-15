cf1c19e0586e942ed242128b3a5e0a32c976c9a3
ALSephirot <asephirotammale@gmail.com>
2015-11-15 17:31:55 -0500

    Estable

---
+++ js/Angular/controllers.js
@@ -0,0 +1,37 @@
+﻿angular.module("OdajuApp", ["OdajuApp.Services"])
+
+.controller("AboutCtrl", function ($scope, OdajuService) {
+    var CallBackR = function (Data) {
+
+        Data.Redes.sort(function (a, b) {
+            if (a.Orden < b.Orden) return -1;
+            if (a.Orden > b.Orden) return 1;
+            return 0;
+        });
+        
+        var redesFInal = $.grep(Data.Redes, function (n) {
+            return n.Pintar == true;
+        })
+
+        Data.Redes = redesFInal;
+
+        $scope.About = Data;
+    };
+
+    var onErrorR = function (Data) {
+        alert(JSON.stringify(Data));
+    }
+
+    OdajuService.GetAbout(CallBackR, onErrorR);
+})
+.controller("NoticiasCtrl", function ($scope, OdajuService) {
+    var CallBackR = function (Data) {
+        $scope.Noticias = Data;
+    };
+
+    var onErrorR = function (Data) {
+        alert(JSON.stringify(Data));
+    }
+
+    OdajuService.GetNoticias(CallBackR, onErrorR);
+})
---
+++ js/Angular/services.js
@@ -0,0 +1,56 @@
+﻿angular.module("OdajuApp.Services", [])
+.factory("OdajuService", function ($http) {
+    var Result = {};
+
+    Result.GetAbout = function (CallBack, onError) {
+        $http.get("../../Data/odaju.json")
+		.success(function (data, status, headers, config) {
+		    CallBack(data.About);
+		})
+        .error(function (data, status, headers, config) {
+            onError(data);
+        });
+    };
+
+    Result.GetNoticias = function (CallBack, onError) {
+        $http.get("../../Data/odaju.json")
+		.success(function (data, status, headers, config) {
+		    CallBack(data.Noticias);
+		})
+        .error(function (data, status, headers, config) {
+            onError(data);
+        });
+    };
+
+    Result.GetVideos = function (CallBack, onError) {
+        $http.get("../../Data/odaju.json")
+		.success(function (data, status, headers, config) {
+		    CallBack(data.RecursosJeloz.Multimedia.Videos);
+		})
+        .error(function (data, status, headers, config) {
+            onError(data);
+        });
+    };
+
+    Result.GetWallPapers = function (CallBack, onError) {
+        $http.get("../../Data/odaju.json")
+		.success(function (data, status, headers, config) {
+		    CallBack(data.RecursosJeloz.WallPapers);
+		})
+        .error(function (data, status, headers, config) {
+            onError(data);
+        });
+    };
+
+    Result.GetCanciones = function (CallBack, onError) {
+        $http.get("../../Data/odaju.json")
+		.success(function (data, status, headers, config) {
+		    CallBack(data.RecursosJeloz.EmisoraOnline);
+		})
+        .error(function (data, status, headers, config) {
+            onError(data);
+        });
+    };
+
+    return Result;
+});
